78e0ef4651ae5cdbc35033d0a403fd90a1ca74f7
DReSA Support <91715679+dresa-org-au@users.noreply.github.com>
2023-08-10 13:25:08 +1000

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
 
   # Should allow token authentication for API calls
   acts_as_token_authentication_handler_for User, except: [:index, :show, :embed, :check_exists, :handle_error, :count,
-                                                          :redirect] 
-  #only: [:new, :create, :edit, :update, :destroy]
+                                                          :redirect] #only: [:new, :create, :edit, :update, :destroy]
+  
   # User auth should be required in the web interface as well; it's here rather than in routes so that it
   # doesn't override the token auth, above.
   before_action :authenticate_user!, except: [:index, :show, :embed, :check_exists, :handle_error, :count, :redirect]
